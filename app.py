c7d25b09d4fc020fa0a19bc0c69a934ddc4e347a
victormartinez-agrae <victor.martinez@agrae.es>
2026-08-31 12:30:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ if columnas_seleccionadas:
         if col in ["FEC_INI", "FEC_FIN"]:
             column_config[col] = st.column_config.DateColumn(format="DD/MM/YYYY")
         elif col != "year" and pd.api.types.is_numeric_dtype(df_filtrado[col]):
-            column_config[col] = st.column_config.NumberColumn(format=" € %.2f")
+            column_config[col] = st.column_config.NumberColumn(format="%.2f €")
     
     st.dataframe(
         df_filtrado[columnas_seleccionadas],
